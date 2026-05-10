cef821e1fb3bb6eae7652b1516dc22be4bac2672
Mayank Golchha <mayank20044002@gmail.com>
2026-05-10 13:02:20 +0530

    Update types.ts

--- feel-the-meal/src/lib/types.ts
+++ feel-the-meal/src/lib/types.ts
@@ -55,7 +55,6 @@ export interface Product {
   description: string;
   price: number;
   categories: ProductCategory[];
-  category: string,
   image_url: string;
   available: boolean;
   available_all_states: boolean;
